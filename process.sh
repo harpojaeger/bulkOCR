20728441d63b46b122755a0305e61b4eefbf773f
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-07-02 10:52:31 +0200

    Use awk to give semi-verbose progress info during mogrify

--- process.sh
+++ process.sh
@@ -4,33 +4,42 @@ width=`tput cols`
 height=`tput lines`
 updatebar ()
 {
-#First param: current number.  Second: total.
-prop=`calc $1/$2`
-percentage=`calc 100*$prop`
-percentage=`int $percentage`
-header="item $1/$2 ($percentage%) "
-headerlength=${#header}
-totalbarlength=`calc $width-$headerlength-2`
-barlength=`calc $totalbarlength*$prop`
-barchars=`int $barlength`
-bar="for i in `seq 1 $barchars`; do printf \#; done;"
-thebar=`eval $bar`
-echo "$header $thebar\r\c"
+  #First param: current number.  Second: total.
+  prop=`calc $1/$2`
+  percentage=`calc 100*$prop`
+  percentage=`int $percentage`
+  header="item $1/$2 ($percentage%) "
+  headerlength=${#header}
+  totalbarlength=`calc $width-$headerlength-2`
+  barlength=`calc $totalbarlength*$prop`
+  barchars=`int $barlength`
+  bar="for i in `seq 1 $barchars`; do printf \#; done;"
+  thebar=`eval $bar`
+  echo "$header $thebar\r\c"
 }
-
-cd all_imgs
-mogrify -identify -path ../cropped -gravity southeast -crop 1710x100+0+0 +repage *
-cd ../cropped
+touch data.txt
+numfiles=`ls -l some_imgs | wc -l`
+mogrify -identify -path cropped -gravity southeast -crop 1710x100+0+0 +repage some_imgs/* | awk -v n="$numfiles" '
+BEGIN {
+  print "Hello world."
+  i=1
+  #n is 1 too big because ls | wc is weird.
+  n--
+}{
+  printf ("%s (%u/%u) \n",$0,i,n)
+  i++
+}
+'
+cd cropped
 numfiles=`ls -1 | wc -l`
 n=1
 Echo "OCRing..."
 for i in $( ls ); do
-tesseract $i $i &> /dev/null
-#cat $i.txt >> data.txt
-read -r FIRSTLINE < $i.txt
-echo $FIRSTLINE >> ~/Desktop/CF\ camera\ photos/data.txt
-rm $i.txt
-updatebar $n $numfiles
-((n++))
+  tesseract $i $i &> /dev/null
+  read -r FIRSTLINE < $i.txt
+  echo $FIRSTLINE >> ../data.txt
+  rm $i.txt
+  updatebar $n $numfiles
+  ((n++))
 done
 echo "\nDone."
